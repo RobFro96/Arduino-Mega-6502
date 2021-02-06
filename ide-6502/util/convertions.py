946df474e8d0beb7bc9a4e682aa0b218f33b4509
Robert Fromm <rob_fro@web.de>
2021-02-06 16:15:26 +0100

    Removed IDE

--- ide-6502/util/convertions.py
+++
@@ -1,7 +0,0 @@
-class Convertions:
-    @classmethod
-    def string_to_int_auto_base(cls, string: str, default=None):
-        try:
-            return int(string, 0)
-        except ValueError:
-            return default
